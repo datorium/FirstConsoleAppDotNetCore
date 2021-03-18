643d43a16d2a23c3a8ff3e23005d18952cee89a3
Datorium <hello@datorium.lv>
2021-03-18 20:18:09 +0200

    HUman class created

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace FirstConsoleAppDotNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human human1 = new Human("Anna", "Lopez", 16);
+            human1.Introduce();
+
+            Human human2 = new Human("John", "Jackson", 40);
+            human2.Introduce();
+
+            Human human3 = new Human("Jenifer", "Branson", 25);
+            human3.Introduce();
         }
     }
+
+    class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Human(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {this.Name} {this.Surname} and I am {this.Age} years old.");
+        }
+    }
+
 }
