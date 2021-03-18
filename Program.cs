1f044be5def648f1e0a74f1c312024b23f5c361c
Datorium <hello@datorium.lv>
2021-03-18 19:08:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleAppDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
